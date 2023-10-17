10afedd969679d1051d6702eeabf2a0adf6b5888
Daniela Ramos <daniramos5566@hotmail.com>
2023-10-17 11:43:12 -0600

    Initial Commit

---
+++ Consultame/ConsultameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsultameApp.swift
+//  Consultame
+//
+//  Created by Daniela Ramos Garcia on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ConsultameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
