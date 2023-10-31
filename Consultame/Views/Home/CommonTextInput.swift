22944cc4333dfd0eea5332ad66cfc76d1c50c6cf
paulinalh <a00833516@tec.mx>
2023-10-31 13:00:45 -0600

    chore: add common text input view

---
+++ Consultame/Views/Home/CommonTextInput.swift
@@ -0,0 +1,20 @@
+//
+//  CommonTextInput.swift
+//  Consultame
+//
+//  Created by Alumno on 31/10/23.
+//
+
+import SwiftUI
+
+struct CommonTextInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommonTextInput_Previews: PreviewProvider {
+    static var previews: some View {
+        CommonTextInput()
+    }
+}
