75a858e1d4f333c2f5d398b4c5e3597afd16958d
Alberto Tamez <69254699+alberto-tamez@users.noreply.github.com>
2023-11-01 16:44:10 -0600

    First Home Draft

--- Consultame/Home/HomeView.swift
+++ Consultame/Home/HomeView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            RoundedRectangle(cornerRadius: 15)
+                .frame(width: 350, height: 60
+                       , alignment: .center)
+                .foregroundColor(.black)
+                .overlay(
+                    HStack{
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                        Text("Empieza una cita")
+                            .foregroundColor(.white)
+                    }
+                )
+                .onTapGesture {
+                    // Function goes here
+                }
+            ZStack{ // TODO:convertir este rectangulo en componente para agregar varias vistas con este tipo de "informational bubble"
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 351, height: 101)
+                    .foregroundColor(.gray)
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: 350, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                    .foregroundColor(.white)
+                    .shadow(color: Color.gray.opacity(0.6), radius: 5, x: 0, y: 5)
+            }
+
+        }
     }
 }
 
