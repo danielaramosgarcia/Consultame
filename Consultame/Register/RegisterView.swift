d6ed8ea60f89949ebfa9c361d7faa19bb6f0c48a
gaelgarcia003 <88355164+gaelgarcia003@users.noreply.github.com>
2023-11-06 15:51:58 -0600

    added view

--- Consultame/Register/RegisterView.swift
+++ Consultame/Register/RegisterView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var nombreCompleto = ""
+    @State var correo = ""
+    @State private var contrasena = ""
+    @State private var contrasenaRepetida = ""
+    @State var sexo = ""
+    let opcionesSexo = ["Hombre", "Mujer", "No especificar"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("register")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 180)
+                .padding()
+            Text("Regístrate")
+                .font(.system(size: 45).bold())
+                .multilineTextAlignment(.center)
+            Text("Crea tu cuenta y comienza a usar la app")
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+            TextField("Nombre completo", text: $nombreCompleto)
+                .textFieldStyle(CommonTextInput(iconImage: "tray"))
+                .padding(.horizontal, 30)
+            TextField("Correo", text: $correo)
+                .textFieldStyle(CommonTextInput(iconImage: "tray"))
+                .padding(.horizontal, 30)
+            SecureField("Contraseña", text: $contrasena)
+                .textFieldStyle(CommonTextInput(iconImage: "lock"))
+                .padding(.horizontal, 30)
+            SecureField("Repetir contraseña", text: $contrasenaRepetida)
+                .textFieldStyle(CommonTextInput(iconImage: "lock"))
+                .padding(.horizontal, 30)
+            Picker("Sexo", selection: $sexo) {
+                        ForEach(opcionesSexo, id: \.self) { sexo in
+                            Text(sexo)
+                        }
+            }
+            Button("Registrarme"){
+                // navegacion de paginas
+            }
+            .buttonStyle(BotonesInicio())
+            .padding(.top, 10)
+        }
+        .padding()
     }
 }
 
-#Preview {
-    RegisterView()
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
 }
