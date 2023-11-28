103bb282c31d1921c95a5d33b0683dcc582d1e43
Daniela Ramos <daniramos5566@hotmail.com>
2023-11-27 19:06:11 -0600

    start core data

---
+++ Consultame/CoreData/DataController.swift
@@ -0,0 +1,13 @@
+//
+//  DataController.swift
+//  Consultame
+//
+//  Created by Daniela Ramos Garcia on 27/11/23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject{
+    let container = n
+}
