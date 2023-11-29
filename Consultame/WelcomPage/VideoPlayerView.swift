898fee63edf690477585e22db2905ceb2a4757bd
a01177702 <a01177702@tec.mx>
2023-11-28 18:58:18 -0600

    Gerardos welcome correct

--- Consultame/WelcomPage/VideoPlayerView.swift
+++ Consultame/WelcomPage/VideoPlayerView.swift
@@ -8,55 +8,39 @@
 import SwiftUI
 import AVKit
 
-//struct VideoPlayerView: UIViewRepresentable {
-//    var videoName: String
-//
-//    func makeUIView(context: Context) -> UIView {
-//        let view = UIView(frame: .zero)
-//
-//        print("Cargando video:", videoName)
-//
-//        guard let path = Bundle.main.path(forResource: videoName, ofType: "mp4") else {
-//            fatalError("Video file not found")
-//        }
-//
-//        print("Ruta del video encontrado:", path)
-//
-//
-//        let player = AVPlayer(url: URL(fileURLWithPath: path))
-//        let layer = AVPlayerLayer(player: player)
-//        layer.frame = view.bounds
-//        layer.videoGravity = .resizeAspectFill
-//        view.layer.addSublayer(layer)
-//
-//        player.play()
-//
-//        print("Reproduciendo video:", videoName)
-//
-//
-//
-//        return view
-//    }
-//
-//    func updateUIView(_ uiView: UIView, context: Context) {
-//        if let layer = uiView.layer.sublayers?.first as? AVPlayerLayer {
-//            layer.frame = uiView.bounds
-//        }
-//    }
-//
-//}
-
 struct VideoPlayerView: View {
-    var videoName: String
-    
-    var player: AVPlayer {
-        let player = AVPlayer(url: Bundle.main.url(forResource: videoName, withExtension: "mp4")!)
-        player.play()
-        return player
+    let videoName: String
+    @State private var player: AVPlayer?
+
+    private func setupPlayer() {
+        guard let url = Bundle.main.url(forResource: videoName, withExtension: "mp4") else {
+            fatalError("Video file not found")
+        }
+
+        let player = AVPlayer(url: url)
+        self.player = player
+
+        // Looping the video
+        NotificationCenter.default.addObserver(
+            forName: .AVPlayerItemDidPlayToEndTime,
+            object: player.currentItem,
+            queue: .main) { [weak player] _ in
+                player?.seek(to: CMTime.zero)
+                player?.play()
+            }
     }
-    
+
     var body: some View {
         VideoPlayer(player: player)
             .aspectRatio(contentMode: .fit)
+            .onAppear {
+                if player == nil {
+                    setupPlayer()
+                }
+                player?.play()
+            }
+            .onDisappear {
+                player?.pause()
+            }
     }
 }
